a28770aa81b8311b4f3e9fa732ed5c4d70e4417b
Jayavel <jayavel1520@gmail.com>
2025-04-24 21:57:43 +0530

    Refactor comments in Main.java for clarity

--- Day 01/Main.java
+++ Day 01/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello this is first Java App!");
+    // This is single line comment
+
+    /*
+        This is multi line comment
+    */
+    System.out.println("Hello this is first Java App!");
+    
     }    
 }
