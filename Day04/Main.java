9539f9b5beb1fb302d9cf47e67852ef49d208a46
Jayavel <jayavel1520@gmail.com>
2025-05-01 21:13:54 +0530

    About the while and do while loops

---
+++ Day04/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        // While loops
+
+        // Initialize a counter variable to 0
+        int count = 0;
+
+        // This while loop will run as long as count is less than or equal to 20
+        while (count <= 20) {
+            // Print the current value of count
+            System.out.println(count);
+
+            // Increment count by 1
+            count++;
+        }
+
+        // Initialize another counter variable to 20
+        int i = 20;
+
+        // This while loop will run as long as i is greater than or equal to 0
+        while (i >= 0) {
+            // Print the current value of i
+            System.out.println(i);
+
+            // Decrement i by 1
+            i--;
+        }
+    }
+}
